a4b3d6fb9dc018e159d911953e205276fc3f0425
刘洪亮 <liuhongliang@jingshonline.com>
2019-08-30 13:14:04 +0800

    配置文件支持通过环境变量设置

--- src/Surging.Core/Surging.Core.DNS/Configurations/EventBusConfigurationExtensions.cs
+++ src/Surging.Core/Surging.Core.DNS/Configurations/EventBusConfigurationExtensions.cs
@@ -34,6 +34,7 @@ namespace Surging.Core.DNS.Configurations
         {
             Check.NotNull(builder, "builder");
             Check.CheckCondition(() => string.IsNullOrEmpty(path), "path");
+            path = EnvironmentHelper.GetEnvironmentVariable(path);
             if (provider == null && Path.IsPathRooted(path))
             {
                 provider = new PhysicalFileProvider(Path.GetDirectoryName(path));
