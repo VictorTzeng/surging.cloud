0e6aae38378f19402ff3a6b49735c43e59e41548
liuhll <1029765111@qq.com>
2019-08-31 15:54:37 +0800

    增加string扩展方法、集合方法、修复代理生成器存在的bug

--- src/Surging.Core/Surging.Core.ProxyGenerator/Implementation/ServiceProxyGenerater.cs
+++ src/Surging.Core/Surging.Core.ProxyGenerator/Implementation/ServiceProxyGenerater.cs
@@ -22,6 +22,7 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
 using Surging.Core.CPlatform;
+using Surging.Core.Domain.PagedAndSorted;
 
 namespace Surging.Core.ProxyGenerator.Implementation
 {
@@ -174,7 +175,8 @@ namespace Surging.Core.ProxyGenerator.Implementation
                     UsingDirective(GetQualifiedNameSyntax(typeof(IRemoteInvokeService).Namespace)),
                     UsingDirective(GetQualifiedNameSyntax(typeof(CPlatformContainer).Namespace)),
                     UsingDirective(GetQualifiedNameSyntax(typeof(ISerializer<>).Namespace)),
-                    UsingDirective(GetQualifiedNameSyntax(typeof(ServiceProxyBase).Namespace))
+                    UsingDirective(GetQualifiedNameSyntax(typeof(ServiceProxyBase).Namespace)),
+                    UsingDirective(GetQualifiedNameSyntax(typeof(IPagedResult<>).Namespace))
                 }.Concat(directives));
         }
 
