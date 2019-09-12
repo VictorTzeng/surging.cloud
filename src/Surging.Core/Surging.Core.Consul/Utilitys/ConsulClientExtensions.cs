c341351c34b10376c96d70bf03befb16cccb2713
liuhll <1029765111@qq.com>
2019-09-12 15:09:25 +0800

    优化从consul服务中心获取元数据的方法

--- src/Surging.Core/Surging.Core.Consul/Utilitys/ConsulClientExtensions.cs
+++ src/Surging.Core/Surging.Core.Consul/Utilitys/ConsulClientExtensions.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using Polly;
 
 namespace Surging.Core.Consul.Utilitys
 {
@@ -12,23 +13,29 @@ namespace Surging.Core.Consul.Utilitys
     {
         public static async Task<string[]> GetChildrenAsync(this ConsulClient client, string path)
         {
+            var policy = Policy.Handle<HttpRequestException>().WaitAndRetry(5, i => TimeSpan.FromMilliseconds(1000));
+
             try
             {
-                var queryResult = await client.KV.List(path);
-                return queryResult.Response?.Select(p => Encoding.UTF8.GetString(p.Value)).ToArray();
-            }
-            catch (HttpRequestException)
-            {
+                return policy.Execute<string[]>(() => {
+                    var queryResult = client.KV.List(path).Result;
+                    return queryResult.Response?.Select(p => Encoding.UTF8.GetString(p.Value)).ToArray();
+                });
+            } catch (HttpRequestException) {
                 return null;
             }
+            
         }
 
         public static async Task<byte[]> GetDataAsync(this ConsulClient client, string path)
         {
+            var policy = Policy.Handle<HttpRequestException>().WaitAndRetry(5, i => TimeSpan.FromMilliseconds(1000));
             try
             {
-                var queryResult = await client.KV.Get(path);
-                return queryResult.Response?.Value;
+                return policy.Execute<byte[]>(() => {
+                    var queryResult = client.KV.Get(path).Result;
+                    return queryResult.Response?.Value;
+                });               
             }
             catch (HttpRequestException)
             {
