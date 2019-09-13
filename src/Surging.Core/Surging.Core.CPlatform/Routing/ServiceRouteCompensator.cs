220384fad8292ddc66065b347efe39459d714324
liuhll <1029765111@qq.com>
2019-09-14 00:49:13 +0800

    新增Service2

---
+++ src/Surging.Core/Surging.Core.CPlatform/Routing/ServiceRouteCompensator.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Timers;
+
+namespace Surging.Core.CPlatform.Routing
+{
+    public class ServiceRouteCompensator
+    {
+        private readonly Timer _timer;
+        private ILogger<ServiceRouteCompensator> _logger;
+        private IServiceRouteProvider _serviceRouteProvider;
+        private int count = 1;
+
+        public ServiceRouteCompensator(ILogger<ServiceRouteCompensator> logger, IServiceRouteProvider serviceRouteProvider)
+        {
+            _logger = logger;
+            _serviceRouteProvider = serviceRouteProvider;
+            _timer = new Timer();
+            _timer.Enabled = true;
+            
+            _timer.Interval = GenerateInterval();
+            _timer.Elapsed += _timer_Elapsed;
+            _timer.Start();
+
+        }
+
+        private int GenerateInterval()
+        {
+            var ro = new Random();
+            var interval = ro.Next(10000, 50000);
+            return interval;
+        }
+
+        private void _timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            
+            if (count <= AppConfig.ServerOptions.CompensationRegisterRoutesCount)
+            {
+                _serviceRouteProvider.RegisterRoutes(Math.Round(Convert.ToDecimal(Process.GetCurrentProcess().TotalProcessorTime.TotalSeconds), 2, MidpointRounding.AwayFromZero)).Wait();
+                if (_logger.IsEnabled(LogLevel.Debug)) {
+                    _logger.LogDebug($"第{count}次进行服务路由补偿注册");
+                }
+                count++;
+            }
+            else
+            {
+                _timer.Stop();
+                _timer.Dispose();
+            }
+
+        }
+    }
+}
---
+++ src/Surging.Services/Surging.Services.Server2/Startup.cs
@@ -0,0 +1,95 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Surging.Core.CPlatform.Runtime.Server;
+using Surging.Core.CPlatform.Utilities;
+using Surging.Core.ProxyGenerator;
+
+namespace Surging.Services.ServiceHost
+{
+    public class Startup
+    {
+        private readonly IConfigurationBuilder _configurationBuilder;
+        private const string updateHostActionRoute = "v1/api/action/initactions";
+        private const int hostNameSegmentLength = 3;
+
+        public Startup(IConfigurationBuilder config)
+        {
+            _configurationBuilder = config;
+        }
+
+        public IContainer ConfigureServices(ContainerBuilder builder)
+        {
+            var services = new ServiceCollection();
+            builder.Populate(services);
+            ServiceLocator.Current = builder.Build();
+            return ServiceLocator.Current;
+        }
+
+        public void Configure(IContainer app)
+        {
+        }
+
+        //internal static void InitActions()
+        //{
+        //    var serviceProxyProvider = ServiceLocator.GetService<IServiceProxyProvider>();
+        //    var serviceEntryProvider = ServiceLocator.GetService<IServiceEntryProvider>();
+        //    var entries = serviceEntryProvider.GetEntries();
+        //    var logger = ServiceLocator.GetService<ILogger<Startup>>();
+        //    var actions = entries.Select(p => new {
+        //        ServiceId = p.Descriptor.Id,
+        //        ServiceHost = GetServiceHost(p.Type.FullName),
+        //        Application = GetApplication(p.Type.FullName),
+        //        WebApi = p.RoutePath,
+        //        Name = p.Descriptor.GetMetadata<string>("GroupName"),
+        //        DisableNetwork = p.Descriptor.GetMetadata<bool>("DisableNetwork"),
+        //        EnableAuthorization = p.Descriptor.GetMetadata<bool>("EnableAuthorization"),
+        //        AllowPermission = p.Descriptor.GetMetadata<bool>("AllowPermission"),
+        //        Developer = p.Descriptor.GetMetadata<string>("Director"), 
+        //        Date = GetDevDate(p.Descriptor.GetMetadata<string>("Date"))
+
+        //    }).ToList();
+        //    var rpcParams = new Dictionary<string, object>() { { "actions", actions } };
+        //    try
+        //    {
+        //        var result = serviceProxyProvider.Invoke<string>(rpcParams, updateHostActionRoute).Result;
+        //        if (result.IsNullOrEmpty())
+        //        {
+        //            logger.LogInformation("≥ı ºªØAction ß∞‹");
+        //        }
+        //        else
+        //        {
+        //            logger.LogInformation(result);
+        //        }
+        //    }
+        //    catch (Exception ex) {
+        //        logger.LogError(ex.Message,ex);
+        //    }
+          
+        //}
+
+        //private static DateTime? GetDevDate(string dateStr)
+        //{
+        //    if (dateStr.IsNullOrEmpty())
+        //    {
+        //        return null;
+        //    }
+        //    return Convert.ToDateTime(dateStr);
+        //}
+
+        //private static string GetApplication(string serviceFullName)
+        //{
+        //    return serviceFullName.Split(".").Last();
+        //}
+
+        //private static string GetServiceHost(string serviceFullName)
+        //{
+        //    return string.Join('.', serviceFullName.Split(".").Take(hostNameSegmentLength));
+        //}
+    }
+}
